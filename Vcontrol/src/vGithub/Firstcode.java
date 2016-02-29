a32287a80cff80f0444c2d352e0aa9798ab0ffcf
e0015288 <e0015288@CR2126.iss.nus.edu.sg>
2016-02-29 15:12:58 +0800

    My first commit 29/2-3:12pm

---
+++ Vcontrol/src/vGithub/Firstcode.java
@@ -0,0 +1,13 @@
+package vGithub;
+
+public class Firstcode {
+
+	public static void main(String[] args) {
+		// First exercise to check in code
+		
+		System.out.println("My first check in");
+		System.out.println("Created a new repository");
+
+	}
+
+}
